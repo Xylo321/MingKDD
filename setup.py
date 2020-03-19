abb470394649d944b219d6b287c83a78415b9667
zswj123 <l2se@sina.cn>
2020-03-19 11:35:09 +0800

    分离出了爬虫

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='reborn-kdd',
+    version='1.0.0',
+    url='',
+    license='',
+    maintainer='zswj123',
+    maintainer_email='l2se@sina.cn',
+    description='',
+    long_description='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "selenium",
+        "requests",
+        "lxml",
+        "cssselect"
+    ]
+)
